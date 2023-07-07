2158f73fa9a9c136f59f72e880b9bd61427d160d
Tolik Petras <toliketras4@gmail.com>
2023-07-07 16:28:20 +0300

    readme

--- src/App.tsx
+++ src/App.tsx
@@ -14,40 +14,40 @@ import { PostDetails } from "./components/PostDetails";
 import { initData } from "./api/data";
 
 export const App: React.FC = () => {
-	const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch();
   const currentProduct = useAppSelector(selectedProduct);
 
-	useEffect(() => {
-		initialProduct(initData);
+  useEffect(() => {
+	initialProduct(initData);
 
-		const data = getProducts();
+	const data = getProducts();
 
-		const newData: Product[] = [];
+	const newData: Product[] = [];
 
-		for (const char in data) {
-			newData.push(data[char]);
-		}
+	for (const char in data) {
+      newData.push(data[char]);
+	}
 
-		dispatch(setProducts(newData));
-	}, []);
+	dispatch(setProducts(newData));
+  }, []);
 
-	return (
-		<main className='section'>
-			<div className='container'>
-				<div className='tile is-ancestor'>
-					<div className='tile is-parent'>
-						<div className='tile is-child box is-success'>
-							<div className='block'>
+  return (
+	<main className='section'>
+      <div className='container'>
+		<div className='tile is-ancestor'>
+          <div className='tile is-parent'>
+			<div className='tile is-child box is-success'>
+              <div className='block'>
                 {currentProduct ? null : <PostsList />}
-							</div>
+              </div>
 
-							<div className='block' data-cy='MainContent'>
+              <div className='block' data-cy='MainContent'>
                 {currentProduct ? <PostDetails /> : null}
               </div>
-						</div>
-					</div>
-				</div>
 			</div>
-		</main>
-	);
+          </div>
+		</div>
+      </div>
+	</main>
+  );
 };
